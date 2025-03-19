5d0e16430fe8bb6d6c515d86f8b6e6bf7e8d1c1c
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-03-19 16:53:58 +0000

    print applications function for molly

--- project/backend.js
+++ project/backend.js
@@ -1,4 +1,11 @@
-let headerText = "Room | Name | Email | Purpose | Timeslot"
+let headerText = "Room | Name | Email | Purpose | Timeslot | Application Number"
+let serial = []
+let application;
+
+function preload(){
+    application = loadJSON("applications.json")
+
+}
 
 function setup(){
     createCanvas(1920,1080);
@@ -10,4 +17,20 @@ function draw(){
     noStroke();
     fill(255);
     text(headerText,50,200)
+}
+
+function printApplications(){
+    let specificY = 250
+    for (let n = 0; n<serial.length; n++){
+        textSize(20)
+        text(applications[0].room[n],60,specificY)
+        text(applications[0].name[n],180,specificY)
+        text(applications[0].email[n],300,specificY)
+        text(applications[0].purpose[n],420,specificY)
+        text(applications[0].time[n],540,specificY)
+        text(applications[0].serial[n],600,specificY)
+        specificY +=50
+        console.log(specificY)
+    }
+
 }
