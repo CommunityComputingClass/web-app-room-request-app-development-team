6850d37b55aa571e47b96387cd059fd53e519144
gozanne <25gavin.ozanne@lwhs.org>
2025-03-28 18:19:18 +0000

    added hover feature

--- project/newTempWorkspace.js
+++ project/newTempWorkspace.js
@@ -1,5 +1,9 @@
+let defaultX = 558
+let defaultY = 270
 let floor2
 let floor3
+let floor2Rooms = []
+let floor3Rooms = []
 
 function preload() {
     floor2 = loadImage("2ndFloor.png")
@@ -10,6 +14,16 @@ function setup() {
     createCanvas(windowWidth - 18, windowHeight - 18);
     background(200);
     imageMode(CENTER);
+    let defaultX = 558
+    let defaultY = 270
+
+    // The following creates the 3rd floor rooms
+    floor3Rooms.push(new room("h301", 0,0, 100, 115))
+    floor3Rooms.push(new room("h303", 100,0, 200, 115))
+    floor3Rooms.push(new room("h304", 300,0, 400, 115))
+    floor3Rooms.push(new room("h305", 400,0, 500, 115))
+    floor3Rooms.push(new room("h306", 500,0, 600, 115))
+    floor3Rooms.push(new room("h307", 600,0, 700, 115))
     
   }
   
@@ -19,27 +33,47 @@ function draw() {
     strokeWeight(1)
     text(mouseX, 10, 10)
     text(mouseY, 10, 20)
-    roomStroke()
-    h101(560, 270)
-    floors()
+    
+    for (let i in floor3Rooms) {
+        floor3Rooms[i].render(0)
+    }
+
+    for (let i in floor3Rooms) {
+        floor3Rooms[i].mouseOver()
+    }
+
     // Nothing here yet – we'll add features step by step
 }
 
 
-function floors() {
-    noFill()
-    rect(390, 270, 865, 260)
-}
+class room {
+    constructor(id, cornerTLX, cornerTLY, cornerBRX, cornerBRY) {
+      this.roomID = id
+      this.building = this.roomID[0] + " Building"
+      this.floor = this.roomID[1] + " Floor"
+      this.x1 = cornerTLX + defaultX
+      this.y1 = cornerTLY + defaultY
+      this.x2 = cornerBRX + defaultX
+      this.y2 = cornerBRY + defaultY
+    }
 
-function h101(x, y) {
-    let localX = x
-    let localY = y
-    roomStroke()
-    rect(localX, localY, 100, 120)
-}
+    render(a) {
+        if (a == 0) {
+            strokeWeight(5)
+            stroke(0)
+            fill(255)
+        } else if (a == 1) {
+            strokeWeight(10)
+            stroke(20)
+            fill(200)
+        }
+        rectMode(CORNERS)
+        rect(this.x1, this.y1, this.x2, this.y2)
+    }
 
-function roomStroke() {
-    strokeWeight(5)
-    stroke(0)
-    fill(255)
+    mouseOver() {
+        if (this.x1 < mouseX && this.x2 > mouseX && this.y1 < mouseY && this.y2 > mouseY) {
+            this.render(1)
+        }
+    }
 }
