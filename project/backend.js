245fab403ee40ac1426cd1ec877cb193b7b413db
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-03-28 18:16:26 +0000

    molly's backend works now, at first. up next: automated email sending

--- project/backend.js
+++ project/backend.js
@@ -1,15 +1,28 @@
-let headerText = "Room | Name | Email | Purpose | Timeslot | Application Number"
-let serial = [0,1,2,3]
-let applications;
+let headerText = "Room        |        Applicant Name | Email of Applicant                          |          Reason for Booking | Time | Application Number"
+let data;
+let buttonApproveRequest
+let buttonDenyRequest
+let ReasonForDenial
+function automatedEmailApproved(){
+
+}
+function automatedEmailDenied(){
+
+}
 
 function preload(){
-    applications = loadJSON("applications.json")
+    data = loadJSON("applications.json")
 
 }
 
 function setup(){
     createCanvas(1920,1080);
     background("white");
+    console.log(data.applications[0])
+   // ReasonForDenial = createInput("answer goes here")
+   // ReasonForDenial.size(300,40)
+   // ReasonForDenial.position(25,150)
+   // ReasonForDenial.input(storeText)
 }
 
 function draw() {
@@ -22,17 +35,25 @@ function draw() {
 
 function printApplications(){
     let specificY = 250
-    for (let n = 0; n<serial.length; n++){
+    for (let n = 0; n<data.applications.length; n++){
         textSize(20)
-        textColor("white")
-        text(applications[n].room,60,specificY)
-        text(applications[n].name,180,specificY)
-        text(applications[n].email,300,specificY)
-        text(applications[n].purpose,420,specificY)
-        text(applications[n].time,540,specificY)
-        text(applications[n].serial,600,specificY)
+        fill("white")
+        text(data.applications[n].room,60,specificY)
+        text(data.applications[n].name,200,specificY)
+        text(data.applications[n].email,360,specificY)
+        text(data.applications[n].purpose,720,specificY)
+        text(data.applications[n].time,920,specificY)
+        text(data.applications[n].serial,1060,specificY)
+        let buttonTextApprove = "approve request"
+        buttonApproveRequest = createButton(buttonTextApprove)
+        buttonApproveRequest.position (1100,specificY)
+        buttonApproveRequest.mousePressed(automatedEmailApproved);
+        let buttontextdeny= "deny request"
+        buttonDenyRequest = createButton(buttontextdeny)
+        buttonDenyRequest.position (1250,specificY)
+        buttonDenyRequest.mousePressed(automatedEmailDenied)
         specificY +=50
-        console.log(specificY)
+        //console.log(specificY)
     }
 
 }
