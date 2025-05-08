b593865a24aac2f5a1cb90dc3d9e948b16ffd00f
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-05-08 21:09:50 +0000

    adding rest of H building now in progress: then the two rooms in the libarary

--- project/newTempWorkspace.js
+++ project/newTempWorkspace.js
@@ -1,15 +1,18 @@
 let defaultX = 400
 let defaultY = 10
-let floor2
-let floor3
+//let floor2
+//let floor3
+let floor1Rooms = []
 let floor2Rooms = []
 let floor3Rooms = []
+let scienceWingLower = []
+let scienceWingFloor2 = []
 let graphics = true
 
-function preload() {
-    floor2 = loadImage("2nd floor.png")
-    floor3 = loadImage("3rd floor.png")
-}
+//function preload() {
+//    floor2 = loadImage("2nd floor.png")
+//    floor3 = loadImage("3rd floor.png")
+//}
 
 function setup() {
     createCanvas(windowWidth - 18, windowHeight - 18);
@@ -32,6 +35,23 @@ function setup() {
     floor2Rooms.push(new room("h205", 665, 0, 765, 115))
     floor2Rooms.push(new room("h206", 765, 0, 865, 115))
     floor2Rooms.push(new room("h207", 410, 170, 470, 260))
+   
+    //the following creates the 2nd floor science rooms and far NE rooms on floor 2
+    scienceWingFloor2.push(new room("Bio Lab 1", 230, 260, 360, 390))
+    scienceWingFloor2.push(new room("Bio Lab 2", 230, 410, 360, 540))
+    scienceWingFloor2.push(new room("h209",230,0,330,100))
+    scienceWingFloor2.push(new room("h211",330,0,430,100))
+    scienceWingFloor2.push(new room("h213", 430, 0, 530, 100))
+
+    // The following creates the 1st floor rooms
+    floor1Rooms.push(new room("h103", 165,0, 265, 115))
+    floor1Rooms.push(new room("h105", 265,0, 365, 115))
+    floor1Rooms.push(new room("h107", 465,0, 565, 115))
+    floor1Rooms.push(new room("h109", 565,0, 665, 115))
+    floor1Rooms.push(new room("h111", 665,0, 765, 115))
+    floor1Rooms.push(new room("h113", 765,0, 865, 115))
+
+    // The following creates the 1st floor science rooms
 }
 
 function draw() {
@@ -41,15 +61,20 @@ function draw() {
     text(mouseY, 10, 20)
 
     if (graphics) {
-        defaultX = 400
-        defaultY = 10
+        //defaultX = 400
+        //defaultY = 10
 
-        render3rd()
+        //render3rd()
         
-        defaultX = 400
-        defaultY = 310
+        //defaultX = 400
+        //defaultY = 310
         
-        render2nd()
+        //render2nd()
+
+        defaultX = 660
+        defaultY = 10
+    
+        render2ndLabs()
     } else {
         refImages()
     }
@@ -180,15 +205,65 @@ function render2nd() {
     }
 }
 
-function mouseClicked() {
-    graphics = !graphics
+function render2ndLabs() {
+    strokeWeight(5)
+    stroke(155)
+    fill(255)
+    rectMode(CORNERS)
+    
+    beginShape() // building outline
+    vertex(defaultX, defaultY)
+    vertex(defaultX + 530, defaultY)
+    vertex(defaultX+530,defaultY+160)
+    vertex(defaultX+300,defaultY+160)
+    vertex(defaultX+300,defaultY+220)
+    vertex(defaultX+300,defaultY+270)
+    vertex(defaultX + 400, defaultY + 580)
+    vertex(defaultX, defaultY + 580)
+    endShape()
+
+    fill(190)
+    
+    beginShape() // history office
+    vertex(defaultX + 130, defaultY + 260)
+    vertex(defaultX + 130, defaultY + 160)
+    vertex(defaultX + 300, defaultY + 160)
+    vertex(defaultX + 300, defaultY + 260)
+    endShape(CLOSE)
+    
+    //beginShape() // world language office
+    //vertex(defaultX + 370, defaultY + 260)
+    //vertex(defaultX + 370, defaultY + 170)
+    //vertex(defaultX + 470, defaultY + 170)
+    //vertex(defaultX + 610, defaultY + 160)
+    //vertex(defaultX + 620, defaultY + 260)
+    //endShape(CLOSE)
+
+    beginShape() // bathroom
+    vertex(defaultX + 130, defaultY + 160)
+    vertex(defaultX + 130, defaultY + 80)
+    vertex(defaultX + 200, defaultY + 80)
+    vertex(defaultX + 200, defaultY + 160)
+    endShape(CLOSE)
+
+    for (let i in scienceWingFloor2) {
+        scienceWingFloor2[i].render(0)
+    }
+    for (let i in scienceWingFloor2) {
+        scienceWingFloor2[i].mouseOver(0)
+    }
 }
 
-function refImages() {
-    defaultX = 400
-    defaultY = 10
-    image(floor3, defaultX - 75, defaultY - 69, floor3.width / 1.245, floor3.height / 1.245) //nice
-    defaultX = 400
-    defaultY = 410
-    image(floor2, defaultX - 160, defaultY - 90, floor3.width / 1.14, floor3.height / 1.15) //nice
-}
+function mouseClicked() {
+    
+//    graphics = !graphics
+} //this has been commented out rather than deleted
+
+//function refImages() {
+//    defaultX = 400
+//    defaultY = 10
+//    image(floor3, defaultX - 75, defaultY - 69, floor3.width / 1.245, floor3.height / 1.245) //nice
+//    defaultX = 400
+//    defaultY = 410
+//    image(floor2, defaultX - 160, defaultY - 90, floor3.width / 1.14, floor3.height / 1.15) //nice
+//}
