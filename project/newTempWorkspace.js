adcb9eb3df9109d2592ae4c364ca50b39bf843df
NGC-1432 <131916088+NGC-1432@users.noreply.github.com>
2025-05-15 17:09:30 +0000

    now you know what room you're looking at

--- project/newTempWorkspace.js
+++ project/newTempWorkspace.js
@@ -1,7 +1,5 @@
 let defaultX = 400
 let defaultY = 10
-//let floor2
-//let floor3
 let floor1Rooms = []
 let floor2Rooms = []
 let floor3Rooms = []
@@ -9,15 +7,16 @@ let scienceWingLower = []
 let scienceWingFloor2 = []
 let graphics = true
 
-//function preload() {
-//    floor2 = loadImage("2nd floor.png")
-//    floor3 = loadImage("3rd floor.png")
-//}
-
 function setup() {
     createCanvas(windowWidth + 30, windowHeight + 30);
     background(200);
     imageMode(CORNER);
+
+    selectedFloor = 3;
+
+    let floorCycleButton = createButton("Cycle selected floor");
+    floorCycleButton.position(200,80);
+    floorCycleButton.mousePressed(cycleFloor);
 
     // The following creates the 3rd floor rooms
     floor3Rooms.push(new room("h301", 165,0, 265, 115))
@@ -26,8 +25,21 @@ function setup() {
     floor3Rooms.push(new room("h305", 565,0, 665, 115))
     floor3Rooms.push(new room("h306", 665,0, 765, 115))
     floor3Rooms.push(new room("h307", 765,0, 865, 115))
+    //library building
+    floor3Rooms.push(new room("L251", 165, 280, 265, 395))
+    floor3Rooms.push(new room("L253", 265, 280, 365, 395))
+    floor3Rooms.push(new room("Photo Studio", 365, 280, 465, 395))
+    //shops pit
+    floor3Rooms.push(new room("Wearables Shop", 165, 450, 265, 550))
+    floor3Rooms.push(new room("Set Design Shop", 265, 450, 365, 550))
+    floor3Rooms.push(new room("Wood Shop", 365, 450, 465, 550))
+    floor3Rooms.push(new room("Computer Lab", 65, 550, 165, 650))
+    floor3Rooms.push(new room("Physics Room", 165, 550, 265, 650))
+    floor3Rooms.push(new room("Electronics Shop", 265, 550, 365, 650))
+    floor3Rooms.push(new room("Metal Shop", 365, 550, 465, 650))
 
     // The following creates the 2nd floor rooms including its science wing
+    floor2Rooms.push(new room("The Center", 0,160, 130, 260))
     floor2Rooms.push(new room("h201", 130, 0, 246, 115))
     floor2Rooms.push(new room("h203", 246, 0, 365, 115))
     floor2Rooms.push(new room("h205", 365, 0, 465, 115))
@@ -42,11 +54,22 @@ function setup() {
     // The following creates the 1st floor rooms
     floor1Rooms.push(new room("h103", 165,0, 265, 115))
     floor1Rooms.push(new room("h105", 265,0, 365, 115))
-    floor1Rooms.push(new room("h107", 465,0, 565, 115))
-    floor1Rooms.push(new room("h109", 565,0, 665, 115))
+    floor1Rooms.push(new room("h107", 365,0, 465, 115))
+    floor1Rooms.push(new room("h109", 465,0, 565, 115))
     floor1Rooms.push(new room("h111", 665,0, 765, 115))
     floor1Rooms.push(new room("h113", 765,0, 865, 115))
+    floor1Rooms.push(new room("Chem Lab 1", 700, 375, 830, 505))
+    floor1Rooms.push(new room("Chem Lab 2", 700, 525, 830, 655))
+    floor1Rooms.push(new room("Dance Studio",320,160,545,400))
 
+}
+
+function cycleFloor(){
+    if (selectedFloor === 3) {
+        selectedFloor = 1;
+    } else {
+        selectedFloor = selectedFloor + 1
+    }
 }
 
 function draw() {
@@ -55,24 +78,19 @@ function draw() {
     text(mouseX, 10, 10)
     text(mouseY, 10, 20)
 
-    if (graphics) {
-        //defaultX = 400
-        //defaultY = 10
-
-        //render3rd()
-        
-        defaultX = 200
-        defaultY = 200
-        
-        render2nd()
-
-        //defaultX = 1395
-       // defaultY = 325
-    
-       // render1st()
-    } else {
-        refImages()
+    defaultX = 200
+    defaultY = 100
+    if (selectedFloor === 3){
+        render3rd()
     }
+    
+    if (selectedFloor === 2){    
+        render2nd()
+    }
+    if (selectedFloor === 1){
+        render1st()
+    }
+    
     
     // Nothing here yet – we'll add features step by step
 }
@@ -87,7 +105,7 @@ class room {
       this.x2 = cornerBRX
       this.y2 = cornerBRY
     }
-
+    
     render(a) {
         if (a == 0) {
             strokeWeight(5)
@@ -105,6 +123,10 @@ class room {
     mouseOver() {
         if (this.x1 + defaultX < mouseX && this.x2 + defaultX > mouseX && this.y1 + defaultY < mouseY && this.y2 + defaultY > mouseY) {
             this.render(1)
+            strokeWeight(5)
+            fill("black")
+            stroke("white")
+            text(this.roomID, this.x2, this.y2)//tells you what room you're hovering your mouse over
         }
     }
 }
@@ -118,7 +140,7 @@ function render3rd() {
     
     fill(190)
     rect(defaultX + 100, defaultY, defaultX + 165, defaultY + 115) // office 1
-    
+
     beginShape() // bottom left offices
     vertex(defaultX + 0, defaultY + 260)
     vertex(defaultX + 0, defaultY + 180)
@@ -145,6 +167,14 @@ function render3rd() {
     vertex(defaultX + 865, defaultY + 165)
     vertex(defaultX + 865, defaultY + 260)
     endShape(CLOSE)
+
+    strokeWeight(1)
+    stroke("black")
+    fill(255)
+
+    text("H Building 3rd Floor", defaultX + 150, defaultY - 20)
+    text("Library (all floors)", defaultX + 0, defaultY + 300)
+    text("Shops Pit", defaultX + 100, defaultY + 500)
 
     for (let i in floor3Rooms) {
         floor3Rooms[i].render(0)
@@ -187,7 +217,7 @@ function render2nd() {
     vertex(defaultX + 290, defaultY + 260)
     endShape(CLOSE)
     
-    beginShape() // teacher lounge
+    beginShape() // teacher lounge and center offices
     vertex(defaultX + 370, defaultY + 260)
     vertex(defaultX + 370, defaultY + 170)
     vertex(defaultX + 470, defaultY + 170)
@@ -225,10 +255,84 @@ function render2nd() {
     }
 }
 
+function render1st() {
+    strokeWeight(5)
+    stroke(155)
+    fill(255)
+    rectMode(CORNERS)
+    
+    beginShape() // building outline
+    vertex(defaultX, defaultY)
+    vertex(defaultX + 865, defaultY)
+    vertex(defaultX + 865, defaultY + 260)
+    vertex(defaultX + 700, defaultY + 260)
+    vertex(defaultX + 700, defaultY + 375)
+    vertex(defaultX + 830, defaultY + 375)
+    vertex(defaultX + 830, defaultY + 505)
+    vertex(defaultX + 800, defaultY + 505)
+    vertex(defaultX + 800, defaultY + 525)
+    vertex(defaultX + 830, defaultY + 655)
+    vertex(defaultX + 100, defaultY + 655)
+    vertex(defaultX + 100, defaultY + 360)
+    vertex(defaultX + 500, defaultY + 360)
+    vertex(defaultX + 500, defaultY + 260)
+    vertex(defaultX, defaultY + 260)
+    endShape()
+
+    fill(190)
+    
+    beginShape() // facilities & restrooms between science wing hall and the dance studio
+    vertex(defaultX + 500, defaultY + 200)
+    vertex(defaultX + 565, defaultY + 200)
+    vertex(defaultX + 565, defaultY + 495)
+    vertex(defaultX + 525, defaultY + 495)
+    vertex(defaultX + 525, defaultY + 535)
+    vertex(defaultX + 565, defaultY + 535)
+    vertex(defaultX + 565, defaultY + 655)
+    vertex(defaultX + 400, defaultY + 655)
+    vertex(defaultX + 400, defaultY + 505)
+    endShape(CLOSE)
+    
+    beginShape() // top left office and elevator
+    vertex(defaultX + 65, defaultY)
+    vertex(defaultX + 165, defaultY)
+    vertex(defaultX + 165, defaultY + 115)
+    vertex(defaultX + 65, defaultY + 115)
+    endShape(CLOSE)
+
+    beginShape() // bottom right offices
+    vertex(defaultX + 700, defaultY + 260)
+    vertex(defaultX + 700, defaultY + 160)
+    vertex(defaultX + 765, defaultY + 165)
+    vertex(defaultX + 865, defaultY + 160)
+    vertex(defaultX + 865, defaultY + 260)
+    endShape(CLOSE)
+
+    beginShape() // science office
+    vertex(defaultX + 700, defaultY + 375)
+    vertex(defaultX + 700, defaultY + 260)
+    vertex(defaultX + 800, defaultY + 260)
+    vertex(defaultX + 800, defaultY + 375)
+    endShape(CLOSE)
+
+    beginShape() // science prep space
+    vertex(defaultX + 700, defaultY + 505)
+    vertex(defaultX + 700, defaultY + 525)
+    vertex(defaultX + 800, defaultY + 525)
+    vertex(defaultX + 800, defaultY + 505)
+    endShape(CLOSE)
+
+    for (let i in floor1Rooms) {
+        floor1Rooms[i].render(0)
+    }
+    for (let i in floor1Rooms) {
+        floor1Rooms[i].mouseOver(0)
+    }
+}
+
 function mouseClicked() {
     
-//    graphics = !graphics
-} //this has been commented out rather than deleted
+}
 
 //function refImages() {
 //    defaultX = 400
